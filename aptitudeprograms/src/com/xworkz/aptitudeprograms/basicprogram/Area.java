c7f48dbca8b8468c4b2653001b844c80e93133d5
User <raghavendra123xworkz@gmail.com>
2022-09-13 00:34:44 +0530

    Aptitude programs of java

---
+++ aptitudeprograms/src/com/xworkz/aptitudeprograms/basicprogram/Area.java
@@ -0,0 +1,12 @@
+package com.xworkz.aptitudeprograms.basicprogram;
+
+public class Area {
+
+	public static void main(String[] args) {
+		int length=10;
+		int width=5;
+		int areaOfRectangle=length*width;
+	System.out.println("areaOfRectangle: "+areaOfRectangle);
+		
+	}
+}
